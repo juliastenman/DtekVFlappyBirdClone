1bd568bb565f7018cb2fde2963a92dd61fd20f2c
alexape <alexape@kth.se>
2024-12-05 15:22:05 +0100

    all test text done

--- analysis/performance_counter.c
+++ analysis/performance_counter.c
@@ -3,7 +3,7 @@ void print_dec(unsigned int);
 void game(void);
 
 int main(){
-    int game_time; // Create a variable called game_time
+    int x; // Create a variable called x
     // Clear the mcycle CSR by writing 0 to it
     asm volatile ("csrw mcycle, x0");
     asm volatile ("csrw minstret, x0");
@@ -16,19 +16,42 @@ int main(){
     asm volatile ("csrw mhpmcounter9, x0");
     // Call the game function
     game();
-    // Read the mcycle value into game_time
-    asm("csrr %0, mcycle" : "=r"(game_time) );
-    asm("csrr %0, minstret" : "=r"(game_time) );
-    asm("csrr %0, mhpmcounter3" : "=r"(game_time) );
-    asm("csrr %0, mhpmcounter4" : "=r"(game_time) );
-    asm("csrr %0, mhpmcounter5" : "=r"(game_time) );
-    asm("csrr %0, mhpmcounter6" : "=r"(game_time) );
-    asm("csrr %0, mhpmcounter7" : "=r"(game_time) );
-    asm("csrr %0, mhpmcounter8" : "=r"(game_time) );
-    asm("csrr %0, mhpmcounter9" : "=r"(game_time) );
-
+    // Read the mcycle value into x
+    asm("csrr %0, mcycle" : "=r"(x) );
     // Print out the value of foo_time (requires print_dec in time4riscv.zip)
     print("\nTime for game() was: ");
-    print_dec(game_time);
+    print_dec(x);
+    print("\n");
+    asm("csrr %0, minstret" : "=r"(x) );
+    print("\nNumber of retired instructions for game() was: ");
+    print_dec(x);
+    print("\n");
+    asm("csrr %0, mhpmcounter3" : "=r"(x) );
+    print("\nNumber of retired memory instructions for game() was: ");
+    print_dec(x);
+    print("\n");
+    asm("csrr %0, mhpmcounter4" : "=r"(x) );
+    print("\nI-cache miss from instruction-fetch for game() was: ");
+    print_dec(x);
+    print("\n");
+    asm("csrr %0, mhpmcounter5" : "=r"(x) );
+    print("\nD-cache miss from memory operation for game() was: ");
+    print_dec(x);
+    print("\n");
+    asm("csrr %0, mhpmcounter6" : "=r"(x) );
+    print("\nNumber of stall from I-cache misses in CPU for game() was: ");
+    print_dec(x);
+    print("\n");
+    asm("csrr %0, mhpmcounter7" : "=r"(x) );
+    print("\nNumber of stall from D-cache misses in CPU for game() was: ");
+    print_dec(x);
+    print("\n");
+    asm("csrr %0, mhpmcounter8" : "=r"(x) );
+    print("\nStalls due to data hazards that cannot be solved by forwarding in the CPU for game() was: ");
+    print_dec(x);
+    print("\n");
+    asm("csrr %0, mhpmcounter9" : "=r"(x) );
+    print("\nNumber of stalls due to expensive ALU operations in CPU for game() was: ");
+    print_dec(x);
     print("\n");
 }
