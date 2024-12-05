6726088b6fac9b8aa1292f49de30b530dd6982e8
alexape <alexape@kth.se>
2024-12-05 15:15:35 +0100

    everything finsihed gg

--- analysis/performance_counter.c
+++ analysis/performance_counter.c
@@ -6,10 +6,27 @@ int main(){
     int game_time; // Create a variable called foo_time
     // Clear the mcycle CSR by writing 0 to it
     asm volatile ("csrw mcycle, x0");
+    asm volatile ("csrw minstret, x0");
+    asm volatile ("csrw mhpmcounter3, x0");
+    asm volatile ("csrw mhpmcounter4, x0");
+    asm volatile ("csrw mhpmcounter5, x0");
+    asm volatile ("csrw mhpmcounter6, x0");
+    asm volatile ("csrw mhpmcounter7, x0");
+    asm volatile ("csrw mhpmcounter8, x0");
+    asm volatile ("csrw mhpmcounter9, x0");
     // Call the game function
     game();
-    // Read the mcycle value into foo_time
+    // Read the mcycle value into game_time
     asm("csrr %0, mcycle" : "=r"(game_time) );
+    asm("csrr %0, minstret" : "=r"(game_time) );
+    asm("csrr %0, mhpmcounter3" : "=r"(game_time) );
+    asm("csrr %0, mhpmcounter4" : "=r"(game_time) );
+    asm("csrr %0, mhpmcounter5" : "=r"(game_time) );
+    asm("csrr %0, mhpmcounter6" : "=r"(game_time) );
+    asm("csrr %0, mhpmcounter7" : "=r"(game_time) );
+    asm("csrr %0, mhpmcounter8" : "=r"(game_time) );
+    asm("csrr %0, mhpmcounter9" : "=r"(game_time) );
+    
     // Print out the value of foo_time (requires print_dec in time4riscv.zip)
     print("\nTime for game() was: ");
     print_dec(game_time);
