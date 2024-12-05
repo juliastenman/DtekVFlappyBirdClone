9ebe87e2c591d4c5919a1ad5654d259d78eb1c17
alexape <alexape@kth.se>
2024-12-05 14:09:38 +0100

    performance_counter.c created

---
+++ performance_counter.c
@@ -0,0 +1,11 @@
+unsigned int foo_time; // Create a variable called foo_time
+// Clear the mcycle CSR by writing 0 to it
+asm volatile (“csrw mcycle, x0”);
+// Call the foo function
+foo();
+// Read the mcycle value into foo_time
+asm("csrr %0, mcycle" : "=r"(foo_time) );
+// Print out the value of foo_time (requires print_dec in time4riscv.zip)
+print(“\nTime for foo() was: “);
+print_dec(foo_time);
+print(“\n”);
